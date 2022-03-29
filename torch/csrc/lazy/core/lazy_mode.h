7a07a9526d7324166ef5f2226ccfc798a0a3a431
Will Constable <whc@fb.com>
2022-03-29 11:06:14 -0700

    Add lazy mode prototype

---
+++ torch/csrc/lazy/core/lazy_mode.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <c10/core/Device.h>
+#include <c10/macros/Export.h>
+
+namespace torch {
+namespace lazy {
+
+TORCH_API bool& in_lazy_mode();
+TORCH_API void LazyModeEnter(c10::Device device);
+TORCH_API void LazyModeExit(c10::Device device);
+
+} // namespace lazy
+} // namespace torch
